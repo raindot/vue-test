58beaa05e65cb5f34010651a63c967fe8bc49b67
Raindot <sakuhime24@gmail.com>
2018-02-27 19:31:51 +0800

    config

---
+++ src/config.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyABPPoZz3rI62WkpOCanHEwNXMakhHxNGY",
+  authDomain: "goodfood-clover.firebaseapp.com",
+  databaseURL: "https://goodfood-clover.firebaseio.com",
+  projectId: "goodfood-clover",
+  storageBucket: "goodfood-clover.appspot.com",
+  messagingSenderId: "45979887609"
+};
